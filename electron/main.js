58957c72e3c1248d3fd22af3e1125dece16a54c7
daijinru <daijinru@360.cn>
2025-08-17 13:24:07 +0800

    🚧 shortcut window from live2d

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { ipcMain } = require('electron');
 
-// app.setName('Wenko');
+app.setName('Wenko');
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
@@ -27,7 +28,7 @@ function createWindow() {
   mainWindow.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
   createWindow();
 
   app.on('activate', () => {
@@ -41,4 +42,35 @@ if (process.platform === 'darwin') {
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
+});
+
+ipcMain.on('wenko_shortcut', async (event, data) => {
+  console.info('wenko_shortcut data received', data);
+  // 处理快捷键事件: action: open
+  const { action } = data;
+  if (action === 'open') {
+    // 打开窗口 800 x 600
+    const shortcutWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      title: 'Wenko ShortCut',
+      icon: path.join(__dirname, 'assets', 'favicon.ico'),
+      frame: true,
+      transparent: false,
+      alwaysOnTop: false,
+      resizable: true,
+      hasShadow: true,
+      fullscreenable: true,
+      skipTaskbar: false,    // 在任务栏显示
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: false,
+        contextIsolation: true,
+        webSecurity: false  // 关闭同源策略和 CSP 检查，方便开发加载任意脚本
+      }
+    });
+    shortcutWindow.loadFile('index.html');
+  } else {
+    console.warn('Unknown action:', action);
+  }
 });
--- electron/preload.js
+++ electron/preload.js
@@ -1,5 +1,11 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  send: (channel, ...args) => ipcRenderer.send(channel, ...args),
+  invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
+});
 
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
