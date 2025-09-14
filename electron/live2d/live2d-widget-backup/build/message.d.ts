70270178925849f30ef324fc06ff6545c641d1ad
daijinru <daijinru@360.cn>
2025-09-14 11:17:53 +0800

    :fire: live2d backup

--- electron/live2d/live2d-widget-backup/build/message.d.ts
+++
@@ -1,9 +0,0 @@
-type Time = {
-    hour: string;
-    text: string;
-}[];
-declare function showMessage(text: string | string[], timeout: number, priority: number, override?: boolean): void;
-declare function showSSEMessage(text: string, id: string, timeout?: number): void;
-declare function welcomeMessage(time: Time, welcomeTemplate: string, referrerTemplate: string): string;
-declare function i18n(template: string, ...args: string[]): string;
-export { showMessage, showSSEMessage, welcomeMessage, i18n, Time };
--- electron/live2d/live2d-widget-backup/build/vector.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const generate: (input: Record<string, any>) => Promise<unknown>;
-export declare const search: (input: Record<string, any>[]) => Promise<unknown>;
